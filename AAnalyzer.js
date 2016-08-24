b11ba92133c924b5f01537fd94f16f85f813d350
Mingzhang <redondoself@gmail.com>
2016-08-24 18:31:51 -0400

    revised AAnalyzer.js

--- AAnalyzer.js
+++ AAnalyzer.js
@@ -1,33 +1,33 @@
 
 var aaDict = {
-    "H":"His",
-    "D":"Asp",
-    "R":"Arg",
-    "F":"Phe",
-    "A":"Ala",
-    "C":"Cys",
-    "G":"Gly",
-    "Q":"Gln",
-    "E":"Glu",
-    "K":"Lys",
-    "L":"Leu",
-    "M":"Met",
-    "N":"Asn",
-    "S":"Ser",
-    "Y":"Tyr",
-    "T":"Thr",
-    "I":"Ile",
-    "W":"Trp",
-    "P":"Pro",
-    "V":"Val",
+    "H": "His",
+    "D": "Asp",
+    "R": "Arg",
+    "F": "Phe",
+    "A": "Ala",
+    "C": "Cys",
+    "G": "Gly",
+    "Q": "Gln",
+    "E": "Glu",
+    "K": "Lys",
+    "L": "Leu",
+    "M": "Met",
+    "N": "Asn",
+    "S": "Ser",
+    "Y": "Tyr",
+    "T": "Thr",
+    "I": "Ile",
+    "W": "Trp",
+    "P": "Pro",
+    "V": "Val",
 };
 
 
 var aaClass = {
-    "Basic":["H", "R", "K"],
-    "Nonpolar (Hydrophobic)":["F", "A", "L", "M", "I", "W", "P", "V"],
-    "Polar, uncharged":["C", "G", "Q", "N", "S", "Y", "T"],
-    "Acidic":["D", "E"]
+    "Basic": ["H", "R", "K"],
+    "Nonpolar (Hydrophobic)": ["F", "A", "L", "M", "I", "W", "P", "V"],
+    "Polar, uncharged": ["C", "G", "Q", "N", "S", "Y", "T"],
+    "Acidic": ["D", "E"]
 };
 
 //Classify amino acides:
@@ -54,10 +54,10 @@ var computeAA = function(seq) {
 
     for (let i = 0; i < SEQ.length; i++) {
         if (SEQ[i] in result) {
-	    result[SEQ[i]] += 1;
-	} else {
-	    result[SEQ[i]] = 1;
-	}
+            result[SEQ[i]] += 1;
+        } else {
+            result[SEQ[i]] = 1;
+        }
     }
 
     return result;
@@ -66,10 +66,10 @@ var computeAA = function(seq) {
 //Prepare data for visualization
 var prepareData = function(result) {
     var data = {
-        "Basic":{"H":0, "R":0, "K":0},
-        "Nonpolar (Hydrophobic)":{"F":0, "A":0, "L":0, "M":0, "I":0, "W":0, "P":0, "V":0},
-        "Polar, uncharged":{"C":0, "G":0, "Q":0, "N":0, "S":0, "Y":0, "T":0},
-        "Acidic":{"D":0, "E":0}
+        "Basic": {"H":0, "R":0, "K":0},
+        "Nonpolar (Hydrophobic)": {"F":0, "A":0, "L":0, "M":0, "I":0, "W":0, "P":0, "V":0},
+        "Polar, uncharged": {"C":0, "G":0, "Q":0, "N":0, "S":0, "Y":0, "T":0},
+        "Acidic": {"D":0, "E":0}
     };
 
     for (let aa in result) {
