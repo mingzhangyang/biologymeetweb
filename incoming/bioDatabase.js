0f1289f1952a0aa40788b5cb18339fe654db5dfb
Mingzhang <redondoself@gmail.com>
2017-05-17 01:31:59 -0400

    add incoming folder for new feature development

---
+++ incoming/bioDatabase.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mingzhang on 5/17/17.
+ */
+'use strict';
+
+const dbs = [{
+  name: '',
+  url: '',
+  tags: [],
+  description: ''
+}, {
+
+}];
+
+const reversedIndex = {};
+
+function addToIndex(db) {
+
+}
+
+const ignored = ['a', 'the', 'this', 'that', 'which', 'for', 'in', 'at', 'on', 'I', 'is', 'was', 'are', 'were', 'by'];
