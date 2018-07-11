ccc18b7b1f6d7df96dcca177abe81f13ef3d6c23
Mingzhang <redondoself@gmail.com>
2018-07-11 14:48:09 -0400

    Update uniprot.js

--- src/searchUniprot/uniprot.js
+++ src/searchUniprot/uniprot.js
@@ -4,7 +4,7 @@
 'use strict';
 
 function search() {
-  var baseUrl = 'http://www.uniprot.org/uniprot/?';
+  var baseUrl = 'https://www.uniprot.org/uniprot/?';
   var opts = '&limit=10&sort=score&columns=id,entry' +
     ' name,protein names,organism,length&format=tab';
   var query = document.getElementById('query').value;
